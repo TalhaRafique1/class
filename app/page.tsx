1ea8f2e3ff17620ea8af5f01606fa21a36f499e9
TalhaRafique1 <162029206+TalhaRafique1@users.noreply.github.com>
2024-10-24 10:54:09 +0500

    work done

---
+++ app/page.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    // <div>
+      
+    //   <h1 className='bg-pink-600	; text-5xl py-6 my-4'> This is heading</h1>
+    //   <p className='bg-orange-700	; text-3xl py-8 m-36 border-8 border-zinc-900 pl-20'>This is paragraph</p>
+    //   <span className='block font-bold ; text-2xl'>This is span</span>
+    //   <button className='p-2 bg-pink-400 m-1 border-yellow-500 border-4 rounded-md '>login</button>
+    // </div>
+
+    // Class 5
+
+    // <div className="bg-[url('/images/download.jpeg')] h-[400px] bg-no-repeat w-full bg-cover">
+    //   <h2 className=' pl-[300px] my-[10px] text-[36px] text-white p-1'>Hello World</h2>
+    // </div>
+
+    <div className='bg-talha bg-no-repeat w-full bg-cover '>
+      <h2 className=' pl-[300px] my-[10px] text-[36px] text-primary p-1'>
+        hello world
+      </h2>
+    </div>
+  )
+}
+
+export default page
+
+
+// px ( pixel per inch)
+// % percantage (percantage of the parent) 
+
+// border is consisted of 3 properties
+// 1. thickness> width
+// 2. style eg: solid, lined, dashed
+// 3. color eg: black, blue, yellow
---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: "#f6bf38",
+        secondary: "#d6300c",
+        talha: "#196e33"
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
